f5e35d7402c29ec53aa7eb4985863d1367e520c8
sebastian.rossetti <sebastian.rossetti@piano.io>
2025-06-21 08:44:20 +0200

    Added second line chart

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
+import pandas
 import polars
 
 from logger import logger
 from src.input_reader import read_input_files
+from src.models.Action import Action
 from src.models.KPI import KPIRequirement, KPIOperation
 from src.settings import SETTINGS
 
@@ -33,7 +35,7 @@ def apply_requirement(dataframe: polars.DataFrame, requirement: KPIRequirement) 
     return dataframe.filter(requirement_condition)
 
 
-def get_chart_data(file: Path = None) -> polars.DataFrame:
+def get_chart_data(file: Path = None) -> (pandas.DataFrame, pandas.DataFrame):
     if file is not None:
         dataframe = polars.read_parquet(file)
         if dataframe is None:
@@ -45,7 +47,8 @@ def get_chart_data(file: Path = None) -> polars.DataFrame:
 
     base_dataframe = dataframe.clone()
 
-    kpi_results = {}
+    general_bar_chart = {}
+    check_line_chart = {}
     actions = dataframe["action"].unique()
     total_weight = dataframe["weight"].sum()
     for kpi in SETTINGS.KPIS:
@@ -58,27 +61,36 @@ def get_chart_data(file: Path = None) -> polars.DataFrame:
 
         if dataframe.shape[0] == 0:
             logger.warning("No rows match the KPI requirements.")
-            kpi_results[kpi.display_name] = {action: 0 for action in actions}
+            general_bar_chart[kpi.display_name] = {action: 0 for action in actions}
+            check_line_chart[kpi.display_name] = {Action.CHECK: 0}
             continue
 
         weight_by_action = {}
         for action in actions:
             weight_by_action[action] = dataframe.filter(dataframe["action"] == action)["weight"].sum()
 
-        kpi_results[kpi.display_name] = {}
+        general_bar_chart[kpi.display_name] = {}
+        check_line_chart[kpi.display_name] = {}
         for action, weight in weight_by_action.items():
             percentage = (weight / total_weight) * 100
-            kpi_results[kpi.display_name][action] = percentage
+            general_bar_chart[kpi.display_name][action] = percentage
             logger.info(f"Percentage of {action}: {percentage:.2f}%")
+        check_line_chart[kpi.display_name][Action.CHECK] = (weight_by_action[Action.CHECK] / total_weight) * 100
 
-        total_percentage = sum(kpi_results[kpi.display_name].values())
+        total_percentage = sum(general_bar_chart[kpi.display_name].values())
         for action in actions:
-            kpi_results[kpi.display_name][action] /= total_percentage
+            general_bar_chart[kpi.display_name][action] /= total_percentage
+        check_line_chart[kpi.display_name][Action.CHECK] /= total_percentage
 
-        kpi_results[f'{kpi.display_name}\n{total_percentage:.2f}%'] = kpi_results[kpi.display_name]
-        del kpi_results[kpi.display_name]
+        general_bar_chart[f'{kpi.display_name}\n{total_percentage:.2f}%'] = general_bar_chart[kpi.display_name]
+        del general_bar_chart[kpi.display_name]
+
+        check_line_chart[f'{kpi.display_name}\n{total_percentage:.2f}%'] = check_line_chart[kpi.display_name]
+        del check_line_chart[kpi.display_name]
 
         dataframe = base_dataframe.join(dataframe, on=base_dataframe.columns, how="anti")
 
-    output = polars.DataFrame(kpi_results)
-    return output.to_pandas()
+    return (
+        polars.DataFrame(general_bar_chart).to_pandas(),
+        polars.DataFrame(check_line_chart).to_pandas()
+    )
--- src/plotter.py
+++ src/plotter.py
@@ -6,30 +6,31 @@ import pandas
 from src.analyzer import get_chart_data
 
 
-def plot_chart(file: Path = None):
-    chart_data = get_chart_data(file=file)
+FIGURE_NUMBER = 0
+
+
+def plot_chart_data(chart_data: pandas.DataFrame,
+                    kind: str,
+                    title: str,
+                    xlabel: str,
+                    ylabel: str,
+                    colors: dict,
+                    show_legend: bool):
     merged = pandas.DataFrame()
     for column in chart_data.columns:
         series = pandas.Series(chart_data[column][0])
         merged[column] = series
     merged = merged.transpose()
-    colors = {
-        "call": "#cc4125",
-        "fold": "#666666",
-        "raise": "#6aa84f",
-        "bet": "#3d85c6",
-        "check": "#f1c232",
-    }
     merged = merged.reindex(colors.keys(), axis=1)
     merged = merged.dropna(axis=1, how="all")
     ax = merged.plot(
-        kind="bar",
+        kind=kind,
         stacked=True,
         figsize=(10, 6),
         color=[colors[col] for col in merged.columns],
         rot=0,
-        xlabel="KPI",
-        ylabel="Distribution",
+        xlabel=xlabel,
+        ylabel=ylabel,
     )
     for container in ax.containers:
         labels = []
@@ -47,7 +48,41 @@ def plot_chart(file: Path = None):
     plt.gca().yaxis.set_major_formatter(
         plt.FuncFormatter(lambda x, _: f"{int(x * 100)}%")
     )
-    plt.title("KPI Comparison")
-    plt.legend(title="KPI")
+    plt.ylim(0, 1)
+    plt.title(title)
+    if show_legend:
+        plt.legend(title=xlabel)
+    else:
+        plt.legend().set_visible(False)
     plt.tight_layout()
+
+
+def plot_chart(file: Path = None):
+    general_bar_chart_data, check_line_chart_data = get_chart_data(file=file)
+    plot_chart_data(
+        chart_data=general_bar_chart_data,
+        kind="bar",
+        title="Action Distribution by KPI",
+        xlabel="KPI",
+        ylabel="Action Distribution",
+        colors={
+            "call": "#cc4125",
+            "fold": "#666666",
+            "raise": "#6aa84f",
+            "bet": "#3d85c6",
+            "check": "#f1c232",
+        },
+        show_legend=True,
+    )
+    plot_chart_data(
+        chart_data=check_line_chart_data,
+        kind="line",
+        title="Check Rate by KPI",
+        xlabel="KPI",
+        ylabel="Check Rate",
+        colors={
+            "check": "#f1c232",
+        },
+        show_legend=False,
+    )
     plt.show()
